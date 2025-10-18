022a24a244951f176e62b25a63dea1b0f6a98430
TianYu <2249244778@qq.com>
2025-10-18 21:54:10 +0800

    1018Sad

--- Assets/Scripts/游戏内容/战斗系统/BossManager.cs
+++ Assets/Scripts/游戏内容/战斗系统/BossManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class BossManager : MonoBehaviour
 {
@@ -9,25 +10,31 @@ public class BossManager : MonoBehaviour
     [Header("数据记录")]
     public BattleInfoRecord_SO battleInfoRecord;
     [Header("Boss信息")]
-    [SerializeField] private float health;
+    [SerializeField] private float maxHealth;
+    [SerializeField] private float currentHealth;
     [SerializeField] private float speed;
     [SerializeField] private float attack;
+    [Header("UI")]
+    [SerializeField] private GameObject health_UI;
     // Start is called before the first frame update
     void Start()
     {
         this.GetComponent<SpriteRenderer>().sprite = BattlePre.Instance.lastBugSprite;
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (health <= 0)
+        UIUpdate();
+        if (currentHealth <= 0)
         {
             Debug.Log("Boss死亡");
             StartCoroutine(LoadScene());
-            
-            
+
+
         }
+        
     }
     
 
@@ -44,15 +51,20 @@ public class BossManager : MonoBehaviour
 
     public void TakeDamage(float damage)
     {
-        this.health -= damage;
+        currentHealth -= damage;
         GetComponent<BossStateCtrl>()?.OnHit(damage); // 打断冲锋
     }
-    
+
 
     IEnumerator LoadScene()
     {
         yield return new WaitForSeconds(1f);
         SceneManager.LoadScene(sceneTo.SceneName);
         Destroy(gameObject);
+    }
+    
+    public void UIUpdate()
+    {
+        health_UI.GetComponent<TMP_Text>().text = "Bug维修进度:\n" + (maxHealth - currentHealth).ToString() + "/" + maxHealth.ToString();
     }
 }
